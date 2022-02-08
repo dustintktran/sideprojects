bd7752437b8f0ff5870ee6ab4e974c72cf92303b
dustintktran <dustintktran@gmail.com>
2022-02-07 20:45:44 -0800

    Initial Commit

---
+++ src/components/tictactoe/tictactoe.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+const TicTacToe = () => {
+
+  const [turn, changeTurn] = useState('X');
+  const [board, changeBoard] = useState([
+    ['X','_','_'],
+    ['_','_','_'],
+    ['_','_','O']
+  ])
+  
+  return (
+    <div className="tic-tac-toe">
+      <h1>Hello world</h1>
+      <div>{board[0]}</div>
+      <div>{board[1]}</div>
+      <div>{board[2]}</div>
+    </div>
+  );
+}
+
+export default TicTacToe;
